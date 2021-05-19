f7fecb43ff26be98e002e25e8b68b9a8882df8c2
Abhishek Dalakoti <abhishek.dalakoti@collegedunia.com>
2021-05-19 11:27:14 +0530

    First Commit

---
+++ first_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from first_app.models import acessRecord,Topic,Webpage
+
+admin.site.register(acessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
+
+
